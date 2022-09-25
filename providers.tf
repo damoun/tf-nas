6fecbc5d3bc2c88272a94f4af6997dd968864aa0
Damien Plénard <damien.plenard@mollie.com>
2022-09-26 01:03:18 +0200

    setup TrueNAS provider

---
+++ providers.tf
@@ -0,0 +1,5 @@
+provider "truenas" {
+  api_key = var.truenas_api_key
+  base_url = var.truenas_base_url
+  debug = true
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "truenas_api_key" {
+  description = "API key for the TrueNAS server"
+}
+
+variable "truenas_base_url" {
+  type = string
+  description = "Base url for the TrueNAS server"
+}
+
+variable "truenas_debug" {
+  type = string
+  description = "Enable debug from the TrueNAS provider"
+}
+
+variable "truenas_pool" {
+  type = string
+  description = "Name of the pool to use on the TrueNAS server"
+}
