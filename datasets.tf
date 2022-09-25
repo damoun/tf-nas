2fa63bd37fc8bc00434fd573c5633fb3f09f4db1
Damien Plénard <damien.plenard@mollie.com>
2022-09-26 01:04:00 +0200

    create zfs dataset time-machine, k8s & torrent

---
+++ datasets.tf
@@ -0,0 +1,31 @@
+resource "truenas_dataset" "backup" {
+    name = "backup"
+    pool = var.truenas_pool
+    comments = "Created by terraform"
+}
+
+resource "truenas_dataset" "time_machine" {
+    name = "time-machine"
+    pool = var.truenas_pool
+    parent = truenas_dataset.backup.name
+    comments = "Created by terraform"
+}
+
+resource "truenas_dataset" "k8s" {
+    name = "kubernetes"
+    pool = var.truenas_pool
+    comments = "Created by terraform"
+}
+
+resource "truenas_dataset" "persistent_volume" {
+    name = "persistent-volume"
+    pool = var.truenas_pool
+    parent = truenas_dataset.k8s.name
+    comments = "Created by terraform"
+}
+
+resource "truenas_dataset" "torrent" {
+    name = "torrent"
+    pool = var.truenas_pool
+    comments = "Created by terraform"
+}
