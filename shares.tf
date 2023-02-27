bde27021e1be03fbc4e6ef63e65ccd17c1f88bdc
Damien Plénard <damien@plenard.me>
2023-02-27 01:28:51 +0100

    create SMB share for time machine & private

---
+++ shares.tf
@@ -0,0 +1,27 @@
+resource "truenas_share_smb" "time_machine" {
+  enabled       = true
+  acl           = true
+  browsable     = true
+  durablehandle = true
+  shadowcopy    = true
+  streams       = true
+  name          = "Time Machine"
+  path_suffix   = "%U"
+  path          = truenas_dataset.time_machine.mount_point
+  purpose       = "ENHANCED_TIMEMACHINE"
+  comment       = "Created by terraform"
+}
+
+resource "truenas_share_smb" "private" {
+  enabled       = true
+  acl           = true
+  browsable     = true
+  durablehandle = true
+  shadowcopy    = true
+  streams       = true
+  name          = "Private"
+  path_suffix   = "%U"
+  path          = truenas_dataset.private.mount_point
+  purpose       = "PRIVATE_DATASETS"
+  comment       = "Created by terraform"
+}
